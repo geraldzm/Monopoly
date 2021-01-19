8a706aae6d44fd53cad231eb4038cbc22980ba64
Juan Jose Vargas Fletes <androidapps0703@gmail.com>
2021-01-19 01:48:23 -0600

    BUGFIXED: Players can buy hotels...

--- src/main/java/com/game/monopoly/Client/controller/PropertyCardController.java
+++ src/main/java/com/game/monopoly/Client/controller/PropertyCardController.java
@@ -8,6 +8,7 @@ package com.game.monopoly.Client.controller;
 import static com.game.monopoly.Client.controller.ServerCommunication.getServerCommunication;
 
 import com.game.monopoly.Client.model.Objects.Player;
+import com.game.monopoly.Client.model.Objects.Players;
 import com.game.monopoly.Client.view.*;
 import com.game.monopoly.common.Comunication.*;
 
@@ -80,12 +81,17 @@ public class PropertyCardController implements IController, MouseListener {
                 System.out.println("Vendiendo propiedad");
                 getServerCommunication().sendMessage(new Message(property.getId(), IDMessage.SELLPROPERTY));
 
+                close();
             } else if (e.getSource().equals(property.buy)){
                 System.out.println("Comprando propiedad");
                 getServerCommunication().sendMessage(new Message(property.getId(), IDMessage.BUYPROPERTY));
+
+                close();
                 
             } else if (e.getSource().equals(property.mortgage)){
                 System.out.println("Comprando mortgage");
+
+                close();
 
             } else if (e.getSource().equals(property.buyHotel)){
                 buyHotel();
@@ -106,29 +112,31 @@ public class PropertyCardController implements IController, MouseListener {
     private void sellHotel() throws IOException {
         System.out.println("vendiendo hotel");
         getServerCommunication().sendMessage(new Message(property.getId(), IDMessage.SELLHOTEL));
-        close();
     }
 
     // Metodo para comprar hotel
     private void buyHotel() throws IOException {
-        if (Player.getInstance().getHouses().get(property.getId()).getAmountHouse() != 4){
+        Players tmp = GameListener.getInstance().getPlayers().get(0);
+
+        if (tmp.getHouses().get(property.getId()).getAmountHouse() != 4){
             JOptionPane.showMessageDialog(property, "Usted aun no tiene 4 casas...");
             return;
         }
 
-        if (Player.getInstance().getHotel().get(property.getId()) != null && Player.getInstance().getHotel().get(property.getId()).getAmountHouse() == 1){
+        if (tmp.getHotel().get(property.getId()) != null && Player.getInstance().getHotel().get(property.getId()).getAmountHouse() == 1){
             JOptionPane.showMessageDialog(property, "Usted ya tiene un hotel en esta propiedad");
             return;
         }
 
         System.out.println("Comprando hotel");
         getServerCommunication().sendMessage(new Message(property.getId(), IDMessage.BUYHOTEL));
-        close();
     }
 
     // Metodo para vender casa
     private void sellHouse() throws IOException {
-        if (!Player.getInstance().getCards().contains(property.getId())){
+        Players tmp = GameListener.getInstance().getPlayers().get(0);
+
+        if (!tmp.getCards().contains(property.getId())){
             JOptionPane.showMessageDialog(property, "Esta carta aun no es tuya...");
             return;
         }
@@ -141,26 +149,24 @@ public class PropertyCardController implements IController, MouseListener {
         System.out.println("vendiendo casa");
 
         getServerCommunication().sendMessage(new Message(property.getId(), IDMessage.SELLHOUSE));
-
-        close();
     }
 
     // Metodo para comprar casa
     private void buyHouse() throws IOException {
+        Players tmp = GameListener.getInstance().getPlayers().get(0);
+
         if (!Player.getInstance().getCards().contains(property.getId())){
             JOptionPane.showMessageDialog(property, "Esta carta aun no es tuya...");
             return;
         }
 
-        if (Player.getInstance().getHouses().get(property.getId()) != null && Player.getInstance().getHouses().get(property.getId()).getAmountHouse() == 4){
+        if (tmp.getHouses().get(property.getId()) != null && tmp.getHouses().get(property.getId()).getAmountHouse() == 4){
             JOptionPane.showMessageDialog(property, "Ya tienes 4 casas en esta propiedad...");
             return;
         }
 
         System.out.println("Comprando casa");
         getServerCommunication().sendMessage(new Message(property.getId(), IDMessage.BUYHOUSE));
-        close();
-
     }
 
     @Override
